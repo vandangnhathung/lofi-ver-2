04c6c7bc9269ea58f67a1fa5e81268ae928d77e9
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-07-19 02:10:00 +0700

    chore(PlayMusicButtons): Refactor PlayMusicButtons component for enhanced functionality.

--- src/components/PlayerMusic/PlayMusicButtons.tsx
+++ src/components/PlayerMusic/PlayMusicButtons.tsx
@@ -1,13 +1,24 @@
-import React, {useRef} from 'react';
-import jazzy1 from "@/assets/musics/jazzy_1.mp3"
+import React, {useRef, useState, useEffect} from 'react';
+import musics from "@/assets/data/musics.json";
 import MenuButton from "@/components/MenuButton/MenuButton";
 import {CirclePause, CirclePlay, SkipBack, SkipForward} from "lucide-react";
 
 const PlayMusicButtons: React.FC = () => {
-    const [isPlay, setIsPlay] = React.useState<boolean>(false);
+    const [isPlay, setIsPlay] = useState<boolean>(false);
     const audioRef = useRef<HTMLAudioElement>(null);
+    const [musicIndex, setMusicIndex] = useState<number>(Math.floor(Math.random() * musics.length));
+    const [currentMusicObject, setCurrentMusicObject] = useState(musics[musicIndex]);
 
-    const handlePlayMusic = () => {
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.src = `/assets/musics/${currentMusicObject.path}`;
+            if (isPlay) {
+                audioRef.current.play();
+            }
+        }
+    }, [currentMusicObject, isPlay]);
+
+    const handlePlaySong = () => {
         if (!isPlay) {
             audioRef.current?.play();
             setIsPlay(true);
@@ -15,17 +26,25 @@ const PlayMusicButtons: React.FC = () => {
             audioRef.current?.pause();
             setIsPlay(false);
         }
+    };
 
-    }
+    const handleNextSong = () => {
+        let nextIndex = musicIndex + 1;
+        if (nextIndex >= musics.length) {
+            nextIndex = 0;
+        }
+        setMusicIndex(nextIndex);
+        setCurrentMusicObject(musics[nextIndex]);
+        setIsPlay(true);
+    };
 
-    console.log(jazzy1);
-
+    console.log(`/assets/musics/${currentMusicObject.path}`);
     return (
         <>
             <MenuButton IconComponent={SkipBack}/>
-            <MenuButton onClick={handlePlayMusic} IconComponent={isPlay ? CirclePause : CirclePlay}/>
-            <MenuButton IconComponent={SkipForward}/>
-            <audio ref={audioRef} loop src={jazzy1}/>
+            <MenuButton onClick={handlePlaySong} IconComponent={isPlay ? CirclePause : CirclePlay}/>
+            <MenuButton onClick={handleNextSong} IconComponent={SkipForward}/>
+            <audio ref={audioRef} loop/>
         </>
     );
 };
