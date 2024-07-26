c16f8603b9a9c4fddde7256df44a49a5be8aca7f
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-07-26 11:15:20 +0700

    feat(SkeletonLoading): Update background color to black for better UX.

--- src/components/Loading/SkeletonLoading.tsx
+++ src/components/Loading/SkeletonLoading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SkeletonLoading = () => {
     return (
-        <div className={`bg-white h-full w-full absolute inset-0`}>
+        <div className={`bg-black h-full w-full absolute inset-0`}>
             <div className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-0.5`}>
                 <div
                     className="animate-spin rounded-full border-t-transparent border-primary border w-10 aspect-square"></div>
