b65c82e5cca6c260cd201fd47748bae08d8185c1
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-07-26 14:21:38 +0700

    feat(SceneButtons): Refactor video element refs and transition logic.

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,4 +1,4 @@
-// components/Scene/Scene.tsx
+// Scene.tsx
 import React, {useEffect, useRef} from 'react';
 import {SceneProps} from "@/components/Scene/Type";
 import {useSelector} from "react-redux";
@@ -10,37 +10,55 @@ const Scene: React.FC<{ scene: SceneProps }> = ({scene}) => {
     // States
     const nightMode = useSelector((state: RootState) => state.scene.nightMode);
     const [currentSceneSrc, setCurrentSceneSrc] = React.useState<string | undefined>(scene.src);
+    const [prevSceneSrc, setPrevSceneSrc] = React.useState<string | undefined>(currentSceneSrc);
+    const [firstRender, setFirstRender] = React.useState<boolean>(true);
 
     // Refs
-    const videoWrapperRef = useRef<HTMLDivElement>(null);
+    const currentVideoWrapperRef = useRef<HTMLVideoElement>(null);
+    const prevVideoWrapperRef = useRef<HTMLVideoElement>(null);
 
     const handleChangeSceneLoading = () => {
-        if (nightMode) {
-            setCurrentSceneSrc(scene?.srcNight);
-            return;
-        }
         setCurrentSceneSrc(scene.src);
     }
 
-    // nightMode, Raining => change scene
     useEffect(() => {
-        const timeline = gsap.timeline();
+        if (!firstRender) {
+            const timeline = gsap.timeline();
 
-        timeline.to(videoWrapperRef.current, {opacity: 0.1, duration: 0.4})
-            .call(() => handleChangeSceneLoading(), [])
-            .to(videoWrapperRef.current, {opacity: 1, duration: 0.5});
+            if (nightMode) {
+                // Fade out current video and fade in previous video
+                timeline
+                    .to(currentVideoWrapperRef.current, {opacity: 0, duration: 2})
+                    .call(() => {
+                        setPrevSceneSrc(scene?.srcNight);
+                    }, [], "-=2")
+                    .to(prevVideoWrapperRef.current, {opacity: 1, duration: 2}, "-=2")
+            } else {
+                // Fade out previous video and fade in current video
+                timeline
+                    .to(prevVideoWrapperRef.current, {opacity: 0, duration: 2})
+                    .call(() => {
+                        setCurrentSceneSrc(scene.src);
+                    }, [], "-=2")
+                    .to(currentVideoWrapperRef.current, {opacity: 1, duration: 2}, "-=2")
+            }
 
-    }, [nightMode])
+        } else {
+            setFirstRender(false);
+        }
+    }, [nightMode, prevSceneSrc]); // Watch for nightMode and prevSceneSrc changes
 
-    // Panel scene change => change scene
+    // Update the scene source when the scene prop changes
     useEffect(() => {
         handleChangeSceneLoading();
     }, [scene]);
 
     return (
-        <div ref={videoWrapperRef} className={`absolute overflow-hidden inset-0 z-10`}>
-            <video src={`/public/assets/videos/${currentSceneSrc}`} autoPlay loop muted
-                   className="object-cover h-full w-full"/>
+        <div className={`absolute overflow-hidden inset-0 z-10`}>
+            <video ref={currentVideoWrapperRef} src={`/public/assets/videos/${currentSceneSrc}`} autoPlay loop muted
+                   className="object-cover inset-0 absolute"/>
+            <video ref={prevVideoWrapperRef} src={`/public/assets/videos/${prevSceneSrc}`} autoPlay loop muted
+                   className="opacity-0 object-cover inset-0 absolute"/>
             <SceneButtons/>
         </div>
     );
--- src/components/Scene/SceneButtons/SceneButtons.tsx
+++ src/components/Scene/SceneButtons/SceneButtons.tsx
@@ -3,7 +3,7 @@ import SceneButton from "@/components/Scene/SceneButtons/SceneButton";
 
 const SceneButtons = () => {
     return (
-        <div className={`absolute inset-0 bg-black`}>
+        <div className={`absolute inset-0`}>
             <SceneButton/>
         </div>
     );
