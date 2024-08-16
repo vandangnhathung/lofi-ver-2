236a8a93aab8d7538ad31c7ba6d859814a5b94ee
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-08-16 11:28:59 +0700

    refactor(ControlMusicList): update icon color

--- src/components/ControlBar/kits/ControlMusic/ControlMusicList.tsx
+++ src/components/ControlBar/kits/ControlMusic/ControlMusicList.tsx
@@ -6,14 +6,16 @@ import categories from "@/assets/data/categories.json";
 const ControlMusicList = () => {
     const musics = useSelector((state: RootState) => state.playerMusic.musics);
     const categorizedMusics = useSelector((state: RootState) => state.playerMusic.categorizedMusics);
-
-    console.log("categories: ", categories, "categorizedMusics: ", categorizedMusics);
+    // console.log("categories: ", categories, "categorizedMusics: ", categorizedMusics);
 
     return (
         <div className="flex gap-4 justify-between">
             {categories.map((category, index) => (
-                <div key={index} className={`w-1/3 aspect-square i`}>
-                    <img className={`w-full h-full`} src={`/assets/icons/${category.src}`} alt=""/>
+                <div key={index} className={`w-1/3 aspect-square rounded-full bg-red-500`}>
+                    <div className={`flex items-center justify-center grayscale h-full`}>
+                        <img className={`w-[35px] aspect-square backdrop-grayscale-0`}
+                             src={`/assets/icons/${category.src}`} alt=""/>
+                    </div>
                 </div>
             ))}
         </div>
