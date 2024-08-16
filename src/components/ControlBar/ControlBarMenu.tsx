01dec85a40bcdc1a56d0d47fd44ac2dbd4cf0fcf
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-08-16 11:18:20 +0700

    refactor(ControlMusicList): update style for lofi & spotify

--- src/components/ControlBar/ControlBarMenu.tsx
+++ src/components/ControlBar/ControlBarMenu.tsx
@@ -3,11 +3,27 @@ import ControlMusic from "@/components/ControlBar/kits/ControlMusic/ControlMusic
 
 const ControlBarMenu = () => {
     return (
-        <div className={`fixed z-20 top-gap-container left-gap-container`}>
+        <div className={`fixed z-20 top-gap-container left-gap-container font-medium`}>
             <div
                 className={`min-w-[400px] max-w-[500px] rounded-3xl border border-[rgba(255,255,255,0.4)] aspect-square backdrop-blur-[4px] bg-[rgba(0,0,0,0.6)] text-white`}>
-                <div className={`pt-5 px-4`}>
-                    <ControlMusic/>
+                <div className={`flex gap-3 pt-5 px-4`}>
+                    <div className={`w-2/3`}>
+                        <ControlMusic/>
+                    </div>
+
+                    {/* lofi & spotify section */}
+                    <div className="w-1/3">
+                        <div className={`flex gap-3 flex-col h-full`}>
+                            <div
+                                className={`border border-[rgba(255,255,255,0.4)] rounded-xl bg-[rgba(255,255,255,0.3)]  backdrop-blur-[4px] h-[50%] ta-center flex items-center justify-center  `}>
+                                lofi.co
+                            </div>
+                            <div
+                                className={`border border-[rgba(255,255,255,0.4)] rounded-xl bg-[rgba(255,255,255,0.3)]  backdrop-blur-[4px] h-[50%] ta-center flex items-center justify-center  `}>
+                                spotify
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/ControlBar/kits/ControlMusic/ControlMusic.tsx
+++ src/components/ControlBar/kits/ControlMusic/ControlMusic.tsx
@@ -3,10 +3,7 @@ import {Guitar} from "lucide-react";
 import ControlMusicList from "@/components/ControlBar/kits/ControlMusic/ControlMusicList";
 
 const ControlMusic = () => {
-    const array = [1, 2, 3, 4]
-    array.forEach(n => console.log(n + 1));
-
-
+  
     return (
         <div
             className={`flex flex-col gap-4 p-2 border border-[rgba(255,255,255,0.4)] rounded-xl bg-[rgba(255,255,255,0.3)] backdrop-blur-[10px]`}>
@@ -14,13 +11,8 @@ const ControlMusic = () => {
                 <p className={`uppercase`}>music</p>
                 <Guitar/>
             </div>
-            <div className={`flex`}>
-                <div className={`w-2/3`}>
-                    <ControlMusicList/>
-                </div>
-                <div className="w-1/3">
-                    lofi.co & spotify
-                </div>
+            <div className={``}>
+                <ControlMusicList/>
             </div>
         </div>
     );
