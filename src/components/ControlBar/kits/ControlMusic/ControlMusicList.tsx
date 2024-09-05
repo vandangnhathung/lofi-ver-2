7e8a812d2c7e896940187e62dd5f661da456b05e
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-09-06 01:29:17 +0700

    refactor(ControlMusicList): add onClick to change categorized song

--- src/components/ControlBar/kits/ControlMusic/ControlMusicList.tsx
+++ src/components/ControlBar/kits/ControlMusic/ControlMusicList.tsx
@@ -4,7 +4,6 @@ import {RootState} from "@/redux/store";
 import categories from "@/assets/data/categories.json";
 import SkeletonLoading from "@/components/Loading/SkeletonLoading";
 import {setCurrentSong} from "@/redux/reducers/playerMusicSlice";
-import {createSelector} from 'reselect';
 
 // Define the props type for the icons
 interface IconProps {
@@ -21,54 +20,6 @@ const loadIcon = (iconName: string) =>
         })
     );
 
-// Selector for currentSong
-const selectCurrentSong = createSelector(
-    (state: RootState) => state.playerMusic.currentSong,
-    (currentSong) => currentSong
-);
-
-const CategoryComponent: React.FC<{
-    category: typeof categories[0];
-    isActive: boolean;
-    onClick: () => void;
-    hoveredCategory: string | null;
-    handleMouseEnter: (categoryName: string) => void;
-    handleMouseLeave: () => void;
-    getIconComponent: (src: string) => React.LazyExoticComponent<React.FC<IconProps>>;
-}> = React.memo(({
-                     category,
-                     isActive,
-                     onClick,
-                     hoveredCategory,
-                     handleMouseEnter,
-                     handleMouseLeave,
-                     getIconComponent
-                 }) => {
-    const IconComponent = getIconComponent(category.src);
-    return (
-        <div
-            className={`group cursor-pointer w-1/3 flex flex-col gap-y-1 items-center ${isActive ? "pointer-events-none" : ""}`}
-            onMouseEnter={() => handleMouseEnter(category.name)}
-            onMouseLeave={handleMouseLeave}
-            onClick={onClick}
-        >
-            <div className="w-full aspect-square rounded-full backdrop-blur-[10px] relative">
-                <div className="relative z-20 flex items-center justify-center h-full">
-                    <Suspense fallback={<SkeletonLoading/>}>
-                        <IconComponent
-                            color={hoveredCategory === category.name || isActive ? "#ffb70b" : "#c3c3c3"}
-                            className="w-[35px] aspect-square"
-                        />
-                    </Suspense>
-                </div>
-                <div
-                    className={`z-10 bg-[rgba(255,255,255,0.2)] opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-500 ease-in-out absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full inset-0 w-[35px] aspect-square`}></div>
-            </div>
-            <p className="ta-center">{category.name}</p>
-        </div>
-    );
-});
-
 const ControlMusicList: React.FC = () => {
     const musics = useSelector((state: RootState) => state.playerMusic.musics);
     const categorizedMusics = useSelector((state: RootState) => state.playerMusic.categorizedMusics);
@@ -79,7 +30,7 @@ const ControlMusicList: React.FC = () => {
         [key: string]: React.LazyExoticComponent<React.FC<IconProps>>
     }>({});
 
-    const currentSong = useSelector(selectCurrentSong);
+    const currentSong = useSelector((state: RootState) => state.playerMusic.currentSong);
 
     const handleMouseEnter = useCallback((categoryName: string) => {
         setHoveredCategory(categoryName);
@@ -103,24 +54,40 @@ const ControlMusicList: React.FC = () => {
     return (
         <div className="flex gap-4 justify-between">
             {categories.map((category, index) => {
-                const isActive = currentSong.category[0] === category.name;
+                const IconComponent = getIconComponent(category.src);
+
                 return (
-                    <CategoryComponent
+                    <div
                         key={index}
-                        category={category}
-                        isActive={isActive}
+                        className={`group cursor-pointer w-1/3 flex flex-col gap-y-1 items-center ${currentSong.category[0] === category.name ? "pointer-events-none" : ""}`}
+                        onMouseEnter={() => handleMouseEnter(category.name)}
+                        onMouseLeave={handleMouseLeave}
                         onClick={() => {
+                            console.log("category clicked", category.name);
                             dispatch(setCurrentSong(categorizedMusics[category.name][0]));
                         }}
-                        hoveredCategory={hoveredCategory}
-                        handleMouseEnter={handleMouseEnter}
-                        handleMouseLeave={handleMouseLeave}
-                        getIconComponent={getIconComponent}
-                    />
+                    >
+                        <div className="w-full aspect-square rounded-full backdrop-blur-[10px] relative">
+                            <div
+                                className="relative z-20 flex items-center justify-center h-full">
+                                <Suspense fallback={<SkeletonLoading/>}>
+                                    <IconComponent
+                                        color={hoveredCategory === category.name || currentSong.category[0] === category.name ? "#ffb70b" : "#c3c3c3"}
+                                        className={`w-[35px] aspect-square`}
+                                    />
+                                </Suspense>
+                            </div>
+
+                            {/* icon hover effect */}
+                            <div
+                                className={`z-10 bg-[rgba(255,255,255,0.2)] opacity-0 group-hover:w-full group-hover:opacity-100 transition-all duration-500 ease-in-out absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full inset-0 w-[35px] aspect-square`}></div>
+                        </div>
+                        <p className="ta-center">{category.name}</p>
+                    </div>
                 );
             })}
         </div>
     );
 };
 
-export default React.memo(ControlMusicList);
+export default ControlMusicList;
