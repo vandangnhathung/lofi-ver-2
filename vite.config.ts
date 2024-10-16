4dc86388218259b15b32dd3085e22d027fc358d5
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-10-16 17:13:53 +0700

    refactor(ci/cd): build

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    define: {
+        global: {
+            basename: process.env.BASE_URL || '/',
+        },
+    },
 });
