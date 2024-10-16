07c33af9458a5574136746a35add3858ec34f89f
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-10-16 18:00:53 +0700

    refactor(global): remove redundant console.log

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(function (_a) {
+    var command = _a.command;
+    var base = command === 'build' ? '/lofi-ver-2/' : '/';
+    return {
+        plugins: [react()],
+        base: base,
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
+        define: {
+            global: {
+                basename: base,
+            },
+        },
+    };
 });
