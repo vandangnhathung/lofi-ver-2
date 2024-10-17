4a5c34e23589a3b51e39c3b2cd3226c9038a62ae
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-10-17 10:40:42 +0700

    refactor(global): update import

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
+// vite.config.ts
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(({command}) => {
-
     const base = command === 'build' ? '/lofi-ver-2/' : '/';
-
     return {
-        plugins: [react()],
+        plugins: [react(), svgr()],
         base,
         resolve: {
             alias: {
-                find: 'src',
                 '@': path.resolve(__dirname, './src'),
             },
         },
-        define: {
-            global: {
-                basename: base,
-            },
+        build: {
+            assetsInlineLimit: 0, // This ensures SVGs are always kept as separate files
         },
-    }
+    };
 });
