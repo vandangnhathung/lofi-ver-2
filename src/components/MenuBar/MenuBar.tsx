37a1242adb14ab51162217fe149df66d84abb436
vandangnhathung <vandangnhathung@gmail.com>
2025-07-28 02:44:02 +0700

    rebuild

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 
 const MenuBar = () => {
     return (
-        <div className={`absolute bottom-4 left-1/2 -translate-x-1/2 z-5- px-gap-container`}>
+        <div className={`absolute bottom-4 left-1/2 z-40 -translate-x-1/2 px-gap-container`}>
             <div
                 className={`flex relative items-center border border-[rgba(255,255,255,0.3)] justify-between w-screen-padding max-w-[1440px] p-2 rounded-xl h-lofi-menu-height before:content-normal backdrop-blur-md bg-[rgba(0,0,0,0.6)] `}>
 
