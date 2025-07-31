9d19097e66aa0d5a902a593c8ac0ba732e5783dc
vandangnhathung <vandangnhathung@gmail.com>
2025-07-31 12:20:22 +0700

    refactor: udpate github url

--- src/components/GitHubButton/GitHubButton.tsx
+++ src/components/GitHubButton/GitHubButton.tsx
@@ -1,10 +1,12 @@
+import githubIcon from "@/assets/icons/github-mark.svg";
+
 const GitHubButton = () => {
     return (
             <a href="https://github.com/vandangnhathung" target="_blank" rel="noopener noreferrer"
             className="p-2 rounded-md hover:bg-[rgba(255,255,255,0.1)] transition-all"
                >
                 <img 
-                    src="/public/assets/icons/github-mark.svg" 
+                    src={githubIcon} 
                     className="w-6 h-6 invert"
                     alt="GitHub" 
                 />
